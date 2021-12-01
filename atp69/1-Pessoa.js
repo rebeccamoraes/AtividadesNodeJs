1bdf7cdced9e25f8a6b48a1e826f2c167fdba997
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-12-01 15:15:24 -0300

    ATP69 - Criando variaveis e imprimindo no console.

---
+++ atp69/1-Pessoa.js
@@ -0,0 +1,8 @@
+const nome = "Lorem";
+const sobrenome = "Ipsum";
+const dataNascimento = "22/01/1989";
+const cpf = "123.456.789-10";
+const rg ="33.234.654-1";
+
+console.log("Nome: ", nome, " ", sobrenome, "\nData de Nascimento: ", dataNascimento,
+    "\nCPF: ", cpf, "\nRG: ", rg);
