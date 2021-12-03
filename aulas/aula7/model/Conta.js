3b9511cd7f93de81c5a57ff94519282343ec0afc
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-12-02 23:18:15 -0300

    Aula7 - Criando construtor;

--- aulas/aula7/model/Conta.js
+++ aulas/aula7/model/Conta.js
@@ -1,35 +1,14 @@
 class Conta {
-    #id;
-    #numero;
-    #agencia;
-    #saldo;
+    id;
+    numero;
+    agencia;
+    saldo;
 
-    get numero() {
-        return this.#numero;
-    }
-    set numero(numero) {
-        this.#numero = numero;
-    }
-
-    get agencia() {
-        return this.#agencia;
-    }
-    set agencia(agencia) {
-        this.#agencia = agencia;
-    }
-
-    get saldo() {
-        return this.#saldo;
-    }
-    set saldo(saldo) {
-        this.#saldo = saldo;
-    }
-
-    get id() {
-        return this.#id;
-    }
-    set id(id) {
-        this.#id = id;
+    constructor(numero, agencia, saldo, id = null) {
+        this.id = id;
+        this.numero = numero;
+        this.agencia = agencia;
+        this.saldo = saldo;
     }
 }
 
