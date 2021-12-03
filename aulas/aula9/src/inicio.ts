de33455e721505e9159d1dec8078fbfe6959a166
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-12-03 15:35:14 -0300

    Aula9 - correção e criando lista.

--- aulas/aula9/src/inicio.ts
+++ aulas/aula9/src/inicio.ts
@@ -5,11 +5,15 @@ console.log('Banco Cap');
 
 const cc1 = new ContaCorrente(15);
 cc1.agencia = "111";
-cc1.numero = 105;
+cc1.numero = "105";
 cc1.depositar(50);
 console.log(cc1);
 
+const lista = new Array<ContaCorrente>();
+lista.push(cc1);
+console.log(lista);
+
 const cp1 = new ContaPoupanca();
-cp1.numero = 298;
+cp1.numero = "298";
 cp1.depositar(259);
 console.log(cp1);
