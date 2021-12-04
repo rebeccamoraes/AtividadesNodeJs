24d58699c829865d7f81900593de6eb50c4e315b
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-12-04 17:20:35 -0300

    ATP74 - Tornando atributos de cliente públicos.

---
+++ atividades/atp74/model/Cliente.js
@@ -0,0 +1,20 @@
+class Cliente {
+    id;
+    nome;
+
+    // get id() {
+    //     return this.#id;
+    // }
+    // set id(id) {
+    //     this.#id = id;
+    // }
+
+    // get nome() {
+    //     return this.#nome;
+    // }
+    // set nome(nome) {
+    //     this.#nome = nome;
+    // }
+}
+
+export default Cliente;
